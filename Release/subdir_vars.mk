b900e11a02372e57361d9522fc6f7573b4dc971f
auro <emaildeauro@gmail.com>
2023-11-16 12:54:36 -0300

    update to work on ubuntu

--- Release/subdir_vars.mk
+++ Release/subdir_vars.mk
@@ -2,8 +2,6 @@
 # Automatically-generated file. Do not edit!
 ################################################################################
 
-SHELL = cmd.exe
-
 # Add inputs and outputs from these tool invocations to the build variables 
 CMD_SRCS += \
 ../2837x_RAM_lnk_cpu1.cmd \
